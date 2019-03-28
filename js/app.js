448f60980055dda0fa2360cc59954b8108b3adbc
David Marchante <cdmarchante@gmail.com>
2019-03-27 22:14:47 -0700

    update js file

--- js/app.js
+++ js/app.js
@@ -1,253 +1,83 @@
 'use strict';
 
+const userNamePrompt = 'Hello and welcome to my site. My name is David, what can I call you?';
+const userName = prompt(userNamePrompt);
+const greetingPrompt = `Nice to meet you, ${userName}. Would you like to play a game to get to know me better?`;
 const questionPrompts = [
   'Am I from Florida?',
+  'Am I Cuban?',
   'Am I fluent in Spanish?',
   'Am I fluent in French',
-  'Am I Cuban?',
   'Is my favroite food Cuban food?'
 ];
-
+const knowDavid = prompt(greetingPrompt).toLowerCase();
 let i = 0;
 
-for (i; i < questionPrompts.length; i++) {
-  // prompt(questionPrompts[i]);
+if (knowDavid === 'yes' || knowDavid === 'y') {
+  for (i; i < questionPrompts.length; i++) {
+    let response = prompt(questionPrompts[i]).toLowerCase();
 
-  console.log(questionPrompts[i]);
+    if (response === 'yes' || response === 'y') {
+      switch (i) {
+      case (i = 0):
+        alert(`Correct ${userName}. I am a rare Florida naitve.`);
+        console.log(`Correct ${userName}. I am a rare Florida naitve.`);
+        break;
+      case (i = 1):
+        alert('I am first-generation Cuban, good guess!');
+        console.log('I am first-generation Cuban, good guess!');
+        break;
+      case (i = 2):
+        alert('Si! I am a native Spanish speaker');
+        console.log('Yes! I am a native Spanish speaker');
+        break;
+      case (i = 3):
+        alert ('Non, but I am conversant in French.');
+        console.log('No, but I am conversant in French.');
+        break;
+      case (i = 4):
+        alert('Of course it is!');
+        console.log('Of course it is!');
+        break;
+      default:
+        alert('Learn more about me on the site!');
+      }
+    } else if (response === 'no' || response === 'n') {
+      switch (i) {
+      case (i = 0):
+        alert('I know it surprising, but I was born andraised in Florida.');
+        console.log('I know it surprising, but I was born andraised in Florida.');
+        break;
+      case (i = 1):
+        alert('I am Cuban, but the odds were on your side!');
+        console.log('I am Cuban, but the odds were on your side!');
+        break;
+      case (i = 2):
+        alert('Being first-generation Cuban I had tospeak to my grand-parents in Spanish.');
+        console.log('Being first-generation Cuban I had tospeak to my grand-parents in Spanish.');
+        break;
+      case (i = 3):
+        alert ('I am not fluent, but I am conversant.');
+        console.log('I am not fluent, but I am conversant.');
+        break;
+      case (i = 4):
+        alert('It is. Cuban food reminds me of home.');
+        console.log('I do. Cuban food reminds me of home.');
+        break;
+      default:
+        alert('Learn more about me on the site!');
+      } 
+    } else {
+      let gameError = 0;
+      while(gameError < 3) {
+        gameError++;
+        alert('Please enter yes/y or no/n');
+        prompt(questionPrompts[i]).toLowerCase();
+      }
+    }
+  }
+} else if (knowDavid === 'no' || knowDavid === 'n') {
+  alert('Ok. Thank you for visiting my site nonetheless');
 }
 
-// Initailizing initial variables
-/*const thankYouMessagePrompt = 'Okay. But thank you for visiting nonetheless.';
-const errorPrompt = 'Please answer with yes/y or no/n. Note: the answers are not case-sensitive.';
-
-let error = false;
-let userName = null;
-
-this.userLogic();
-
-function userLogic(error) {
-  const userPrompt = 'Hello, and welcome to my site! You can call me David, what can I call you?';
-
-  if(error === true) {
-    userName = prompt(`Please enter a value. ${userPrompt}`);
-  } else {
-    userName = prompt(userPrompt);
-  }
-  userResponseLogic(userName);
-}
-
-function userResponseLogic(response) {
-  error = false;
-
-  if (response) {
-    knowLogic();
-  } else {
-    error = true;
-
-    userLogic(error);
-  }
-}
-
-function knowLogic(error) {
-  let knowResponse = null;
-  const getToKnowPrompt = `Hey ${userName}, would like to get to know me?`;
-
-  if(error === true) {
-    knowResponse = prompt(`${errorPrompt} ${getToKnowPrompt}`).toLowerCase();
-  } else {
-    knowResponse = prompt(getToKnowPrompt).toLowerCase();
-  }
-
-  knowResponseLogic(knowResponse);
-}
-
-function knowResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    stateLogic();
-  } else if (response === 'no' || response === 'n') {
-    const thankYouMessage = thankYouMessagePrompt;
-
-    alert(thankYouMessage);
-  } else {
-    error = true;
-
-    knowLogic(error);
-  }
-}
-
-function stateLogic(error) {
-  let stateResponse = null;
-  const homeStatePrompt = 'Great! Am I from Florida?';
-
-  if(error === true) {
-    stateResponse = prompt(`${errorPrompt} ${homeStatePrompt}`).toLowerCase();
-  } else {
-    stateResponse = prompt(homeStatePrompt).toLowerCase();
-  }
-
-  stateResponseLogic(stateResponse);
-}
-
-function stateResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    const correctState = 'Yes! I am from Florida.';
-
-    console.log(correctState);
-
-    spanishLogic(error, correctState);
-  } else if (response === 'no' || response === 'n') {
-    const incorrectState = 'I am from Florida, but nice try.';
-
-    console.log(incorrectState);
-
-    spanishLogic(error, incorrectState);
-  } else {
-    error = true;
-
-    stateLogic(error);
-  }
-}
-
-function spanishLogic(error, state) {
-  let spanishResponse = null;
-  const spanishLanguagePrompt = 'Am I fluent in Spanish?';
-
-  if (error === true) {
-    spanishResponse = prompt(`${errorPrompt} ${spanishLanguagePrompt}`).toLowerCase();
-  } else {
-    spanishResponse = prompt(`${state} ${spanishLanguagePrompt}`).toLowerCase();
-  }
-
-  spanishResponseLogic(spanishResponse);
-}
-
-function spanishResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    const correctSpanish = 'Yes! I do speak Spanish.';
-
-    console.log(correctSpanish);
-
-    frenchLogic(error, correctSpanish);
-  } else if (response === 'no' || response === 'n') {
-    const incorrectSpanish = 'I do speak Spanish, but nice try.';
-
-    console.log(incorrectSpanish);
-
-    frenchLogic(error, incorrectSpanish);
-  } else {
-    error = true;
-
-    spanishLogic(error);
-  }
-}
-
-function frenchLogic(error, spanish) {
-  let frenchResponse = null;
-  const frenchLanguagePrompt = 'Am I fluent French?';
-
-  if (error === true) {
-    frenchResponse = prompt(`${errorPrompt} ${frenchLanguagePrompt}`).toLowerCase();
-  } else {
-    frenchResponse = prompt(`${spanish} ${frenchLanguagePrompt}`).toLowerCase();
-  }
-
-  frenchResponseLogic(frenchResponse);
-}
-
-function frenchResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    const incorrectFrench = 'I speak a a good amount of French, but not enough enough to call myself fluent.';
-
-    console.log(incorrectFrench);
-
-    familyOriginLogic(error, incorrectFrench);
-  } else if (response === 'no' || response === 'n') {
-    const correctFrench = 'Correct! I do not speak French fluently.';
-
-    console.log(correctFrench);
-
-    familyOriginLogic(error, correctFrench);
-  } else {
-    error = true;
-
-    frenchLogic(error);
-  }
-}
-
-function familyOriginLogic(error, french) {
-  let familyOriginResponse = null;
-  const familyOriginPrompt = 'Am I Cuban?';
-
-  if(error === true){
-    familyOriginResponse = prompt(`${errorPrompt} ${familyOriginPrompt}`).toLowerCase();
-  } else {
-    familyOriginResponse = prompt(`${french} ${familyOriginPrompt}`).toLowerCase();
-  }
-
-  familyOriginResponseLogic(familyOriginResponse);
-}
-
-function familyOriginResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    const correctFamilyOrigin = 'Yes! I am a first-generation Cuban.';
-
-    console.log(correctFamilyOrigin);
-
-    foodLogic(error, correctFamilyOrigin);
-  } else if (response === 'no' || response === 'n') {
-    const incorrectFamilyOrigin = 'I am Cuban, but the odds were on your side.';
-
-    console.log(incorrectFamilyOrigin);
-
-    foodLogic(error, incorrectFamilyOrigin);
-  } else {
-    error = true;
-
-    familyOriginLogic(error);
-  }
-}
-
-function foodLogic(error, familyOrigin) {
-  let foodResponse = null;
-  const favoriteFoodPrompt = 'Is my favorite food Cuban food?';
-
-  if (error === true) {
-    foodResponse = prompt(`${errorPrompt} ${favoriteFoodPrompt}`).toLowerCase();
-  } else {
-    foodResponse = prompt(`${familyOrigin} ${favoriteFoodPrompt}`).toLowerCase();
-  }
-
-  foodResponseLogic(foodResponse);
-}
-
-function foodResponseLogic(response) {
-  error = false;
-
-  if (response === 'yes' || response === 'y') {
-    const correctFood = 'Of course I love Cuban food.';
-
-    console.log(correctFood);
-
-    alert(correctFood);
-  } else if (response === 'no' || response === 'n') {
-    const incorrectFood = 'I would be an anomaly if I did not love Cuban food since I am Cuban.';
-
-    console.log(incorrectFood);
-
-    alert(incorrectFood);
-  } else {
-    error = true;
-
-    foodLogic(error);
-  }
-}*/
+console.log(questionPrompts[i]);

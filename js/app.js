9b5df0f09938f7f440b23fa984215488d65f8917
David Marchante <cdmarchante@gmail.com>
2019-03-31 23:09:22 -0700

    made final changes to and bug fixes to codebase

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ const questionPrompts = [
   'Am I fluent in French?',
   'Is my favroite food Cuban food?',
   'What is my favorite number?',
-  'What countries are represented in my household?',
+  'What countries are represented in my household (besides United States and Cuba)?',
 ];
 
 introductionGame();
@@ -26,7 +26,7 @@ function introductionGame() {
       } else if (question === 5) {
         numberResponse(question);
       } else if (question === 6) {
-        countryResponse();
+        countryResponse(question);
       }
     }
   } else if (knowDavid === 'no' || knowDavid === 'n') {
@@ -123,73 +123,151 @@ function negativeResponse(question, response) {
 }
 
 function numberResponse(question) {
-  const favoriteNumberPrompt = 'What is my favorite number?';
+  let attemptNumber = 1;
+  let responseNumber = prompt(`${questionPrompts[question]} You are attempting try ${attemptNumber} out of 3.`);
 
-  let attemptsFavoriteNumber = 1;
-  while (attemptsFavoriteNumber <= 3) {
-    let responseNumber = prompt(`${favoriteNumberPrompt} You are attempting try ${attemptsFavoriteNumber} out of 3.`);
-    attemptsFavoriteNumber++;
-    if (responseNumber < 9) {
+  if (responseNumber == 9) {
+    alert('You got it');
+    console.log(questionPrompts[question]);
+    console.log(responseNumber);
+    console.log('You got it.');
+  } else if (responseNumber < 9) {
+    alert('Too low.');
+    console.log(questionPrompts[question]);
+    console.log(responseNumber);
+    console.log('Too low.');
+    numberResponseAttempt(attemptNumber, question);
+  } else if (responseNumber < 9) {
+    alert('Too low.');
+    console.log(questionPrompts[question]);
+    console.log(responseNumber);
+    console.log('Too high.');
+    numberResponseAttempt(attemptNumber, question);
+  }
+}
+
+function numberResponseAttempt(attempt, question) {
+  attempt++;
+
+  while (attempt <= 3) {
+    let responseNumber = prompt(`${questionPrompts[question]} You are attempting try ${attempt} out of 3.`);
+    if (responseNumber == 9) {
+      alert('You got it');
+      console.log(questionPrompts[question]);
+      console.log(responseNumber);
+      console.log('You got it.');
+      attempt = 4;
+    } else if (responseNumber < 9) {
       alert('Too low.');
-      console.log(favoriteNumberPrompt);
+      console.log(questionPrompts[question]);
       console.log(responseNumber);
       console.log('Too low.');
+      attempt++;
     } else if (responseNumber > 9) {
       alert('Too high');
-    } else if (responseNumber === 9) {
-      alert('You got it');
-      break;
+      console.log(questionPrompts[question]);
+      console.log(responseNumber);
+      console.log('Too high.');
+      attempt++;
     } else {
       errorNumberResponse(question);
     }
   }
 }
 
-function countryResponse() {
-  const contryRepresentationPrompt = 'Guess a countries that is represented in my household (besides United States and Cuba)?';
+function countryResponse(question) {
   const countries = [
-    'Dominican Republic',
-    'France',
-    'Italy',
-    'Tunisia',
+    'dominican republic',
+    'france',
+    'italy',
+    'tunisia',
   ];
-
+  let validResponse = false;
   let attemptsCountry = 1;
+  let countryResponse = prompt(`${questionPrompts[question]} You are attempting try ${attemptsCountry} out of 3.`).toLowerCase();;
 
-  let countryResponse = prompt(`${contryRepresentationPrompt} You are attempting try ${attemptsCountry} out of 3.`);
-  while (attemptsCountry < 3) {
-    attemptsCountry++;
-    for(let country = 1; country < countries.length; country++){
-      if (countryResponse === countries[country]){
-        switch(countryResponse) {
-        case ('Dominican Republic'):
-          alert('Yes! I am half Dominican');
-          console.log(contryRepresentationPrompt[country]);
-          console.log(countryResponse);
-          console.log('Yes! I am half Dominican');
-          break;
-        case ('France'):
-          alert('Yes! Everyone in my household are French citiznes except me.');
-          console.log(contryRepresentationPrompt[country]);
-          console.log(countryResponse);
-          console.log('Yes! Everyone in my household are French citizens except me.');
-          break;
-        case ('Italy'):
-          alert('Yes! My better half\'s background is Italian.');
-          console.log(contryRepresentationPrompt[country]);
-          console.log(countryResponse);
-          console.log('Yes! My better half\'s background is Italian.');
-          break;
-        case ('Tunisia'):
-          alert('My wife\'s family moved from Italy via Tunisia.');
-          console.log(contryRepresentationPrompt[country]);
-          console.log(countryResponse);
-          console.log('My wife\'s family moved from Italy via Tunisia.');
-          break;
-        default:
-          alert(`Try again. ${countryResponse}`);
-        }
+  for(let country = 0; country < countries.length; country++){
+    if (countryResponse === countries[country]) {
+      validResponse = true;
+    }
+  }
+
+  if (validResponse) {
+    switch(countryResponse) {
+    case ('dominican republic'):
+      alert('Yes! I am half Dominican');
+      console.log(questionPrompts[question]);
+      console.log(countryResponse);
+      console.log('Yes! I am half Dominican');
+      break;
+    case ('france'):
+      alert('Yes! Everyone in my household are French citiznes except me.');
+      console.log(questionPrompts[question]);
+      console.log(countryResponse);
+      console.log('Yes! Everyone in my household are French citizens except me.');
+      break;
+    case ('italy'):
+      alert('Yes! My better half\'s background is Italian.');
+      console.log(questionPrompts[question]);
+      console.log(countryResponse);
+      console.log('Yes! My better half\'s background is Italian.');
+      break;
+    case ('tunisia'):
+      alert('My wife\'s family moved from Italy via Tunisia.');
+      console.log(questionPrompts[question]);
+      console.log(countryResponse);
+      console.log('My wife\'s family moved from Italy via Tunisia.');
+      break;
+    }
+  } else {
+    countryResponseAttempt(question, attemptsCountry, countries, validResponse);
+  }
+}
+
+function countryResponseAttempt(question, attempt, countries, validResponse) {
+  attempt++;
+
+  while (attempt <= 3) {
+    let countryResponse = prompt(`Sorry, try again. ${questionPrompts[question]} You are attempting try ${attempt} out of 3.`);
+    for(let country = 0; country < countries.length; country++){
+      if (countryResponse === countries[country]) {
+        validResponse = true;
       }
+    }
+
+    if (validResponse){
+      switch(countryResponse) {
+      case ('dominican republic'):
+        alert('Yes! I am half Dominican');
+        console.log(questionPrompts[question]);
+        console.log(countryResponse);
+        console.log('Yes! I am half Dominican');
+        attempt = 4;
+        break;
+      case ('france'):
+        alert('Yes! Everyone in my household are French citiznes except me.');
+        console.log(questionPrompts[question]);
+        console.log(countryResponse);
+        console.log('Yes! Everyone in my household are French citizens except me.');
+        attempt = 4;
+        break;
+      case ('italy'):
+        alert('Yes! My better half\'s background is Italian.');
+        console.log(questionPrompts[question]);
+        console.log(countryResponse);
+        console.log('Yes! My better half\'s background is Italian.');
+        attempt = 4;
+        break;
+      case ('tunisia'):
+        alert('My wife\'s family moved from Italy via Tunisia.');
+        console.log(questionPrompts[question]);
+        console.log(countryResponse);
+        console.log('My wife\'s family moved from Italy via Tunisia.');
+        attempt = 4;
+        break;
+      }
+    } else {
+      attempt++;
     }
   }
 }
@@ -201,10 +279,5 @@ function errorBoolResponse(question) {
 
 function errorNumberResponse(question) {
   alert('Please enter a number');
-  prompt(questionPrompts[question]).toLowerCase();
-}
-
-function errorCountryResponse(question) {
-  alert('Please enter a the name a value of a country');
   prompt(questionPrompts[question]).toLowerCase();
 }
